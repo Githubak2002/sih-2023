e6d3b335cffe0301a6d8c9e09258b471c772e3e8
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-08-30 21:11:11 +0530

    commit

--- Wbsite Frontend/src/components/Home.jsx
+++ Wbsite Frontend/src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
       {/* === chatobt img === */}
         <Link to="https://drive.google.com/drive/folders/136wvKezTFu3HTFQYsVhdgM6D8Em4cfGd?usp=drive_link" target='blank'>
-        <img className=' bg-[#00000038] p-2 rounded-full fixed h-10 w-10 bottom-5 right-5 sm:bottom-10 sm:right-10' src={chatBot} alt="chatBot" />
+        <img className=' bg-[#00000038] p-2 rounded-full fixed h-14 w-14 bottom-5 right-5 sm:bottom-10 sm:right-10' src={chatBot} alt="chatBot" />
         </Link>
      
 
